ed7f70146f2e2b085ad2bd086f776fdff74b4c2c
alpha101ml-hub <alpha101ml@gmail.com>
2026-01-04 20:01:33 +0530

    Add files via upload

---
+++ bitwise.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 4, b=8, c=7 , d=3;
+
+    // cout<<(a & b)<<endl;
+    // cout<<(a | b)<<endl;
+    // cout<<(a ^ b)<<endl;
+    // cout<<(d ^ c)<<endl;
+    // cout<<(4 << 1)<<endl;
+    // cout<<(10 << 2)<<endl;
+    // cout<<(10 >> 1)<<endl;
+    // cout<<(8 >> 2)<<endl;
+    cout<<(10 >> 1)<<endl;
+    cout<<(10 << 2)<<endl;
+    cout<<(6 & 10)<<endl;
+    cout<<(6 | 10)<<endl;
+    cout<<(6 ^ 10)<<endl;
+    cout<<((5 - 2) * 6)<<endl;
+
+    return 0;
+}
